6c791babf741799142d47a72cfb3e6f1f459a626
francois <totofrancois12@gmail.com>
2023-12-17 00:03:35 +0100

    Add : cardIsDifferent and handIsDifferent test

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -3,28 +3,10 @@ package org.example;
 public class Card {
     CardValue cardvalue;
     Suit suit;
-    Color color;
 
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public Card(CardValue cardvalue, Suit suit, Color color) {
+    public Card(CardValue cardvalue, Suit suit) {
         this.cardvalue = cardvalue;
         this.suit=suit;
-        this.color = color;
-    }
-
-    public void setSuit(Suit suit) {
-        this.suit = suit;
-    }
-
-    public void setCardvalue(CardValue cardvalue) {
-        this.cardvalue = cardvalue;
     }
 
     public Suit getSuit() {
--- src/test/java/PokerTest.java
+++ src/test/java/PokerTest.java
@@ -10,13 +10,55 @@ import java.util.List;
 
 public class PokerTest {
     @Test
-    public void test(){
+    public void cardCount(){
         List<Card> cardlisteOne = Arrays.asList(new Card(CardValue.JACK, Suit.H),new Card(CardValue.ACE,Suit.D),new Card(CardValue.THREE,Suit.C),new Card(CardValue.FOUR,Suit.S),new Card(CardValue.FIVE,Suit.D));
+        List<Card> cardlisteTow = Arrays.asList(new Card(CardValue.JACK, Suit.H),new Card(CardValue.ACE,Suit.D),new Card(CardValue.THREE,Suit.C),new Card(CardValue.FOUR,Suit.S),new Card(CardValue.FIVE,Suit.D));
+
+        PckerHand playerOne = new PckerHand(1, cardlisteOne);
+        PckerHand playerTow = new PckerHand(2, cardlisteTow);
+
+        Assert.assertEquals(playerOne.getSize(),5);
+        Assert.assertEquals(playerTow.getSize(),5);
+    }
+
+    @Test
+    public void cardIsDifferent(){
+        List<Card> cardlisteOne = Arrays.asList(new Card(CardValue.ACE, Suit.H),new Card(CardValue.ACE,Suit.D),new Card(CardValue.THREE,Suit.C),new Card(CardValue.FOUR,Suit.C),new Card(CardValue.FIVE,Suit.D));
 
         PckerHand playerOne = new PckerHand(1, cardlisteOne);
 
-        Assert.assertEquals(playerOne.getSize(),5);
+        boolean areAllDifferent = true;
+
+        for (int i = 0; i < cardlisteOne.size(); i++) {
+            for (int j = i + 1; j < cardlisteOne.size(); j++) {
+                //System.out.println(cardlisteOne.get(i).getCardvalue()+" "+cardlisteOne.get(j).getCardvalue() );
+                if (cardlisteOne.get(i).getCardvalue().equals(cardlisteOne.get(j).getCardvalue())  && cardlisteOne.get(i).getSuit().equals(cardlisteOne.get(j).getSuit())) {
+                    areAllDifferent = false;
+                    break;
+                }
+            }
+        }
+        Assert.assertTrue(areAllDifferent);
     }
 
+    @Test
+    public void handIsDifferent(){
 
+        List<Card> cardlisteOne = Arrays.asList(new Card(CardValue.JACK, Suit.H),new Card(CardValue.ACE,Suit.D),new Card(CardValue.THREE,Suit.C),new Card(CardValue.FOUR,Suit.S),new Card(CardValue.FIVE,Suit.D));
+        List<Card> cardlisteTow = Arrays.asList(new Card(CardValue.JACK, Suit.D),new Card(CardValue.ACE,Suit.H),new Card(CardValue.THREE,Suit.D),new Card(CardValue.FOUR,Suit.C),new Card(CardValue.FIVE,Suit.S));
+
+        boolean areDifferent = true;
+
+        for (Card cardHandOne : cardlisteOne) {
+            for (Card cardHandTow : cardlisteTow) {
+                //System.out.println(cardHandOne.getSuit());
+                if (cardHandOne.getCardvalue().equals(cardHandTow.getCardvalue()) && cardHandOne.getSuit().equals(cardHandTow.getSuit())) {
+                    areDifferent = false;
+                    break;
+                }
+            }
+        }
+
+        Assert.assertTrue(areDifferent);
+    }
 }
